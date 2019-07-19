14864f101cb3923cf92b625f3b5866a9447d4d68
anudeep <yogeshpahilwancapgemini@gmail.com>
2019-07-19 13:50:38 +0530

    updated index with response message

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/',(req,res)=>{
-  res.status(200).send('Hello world from service 1');
+  res.status(200).send('Hello world from service-1');
 });
 
 app.get('/service1',(req,res)=>{
-    res.status(200).send('Hello World updated from service 1 method');
+    res.status(200).send('responding from service 1 service');
 });
-app.get('/service1/number',(req,res)=>{
-  res.status(200).send('Always Number one');
-});
+
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
   console.log(`Server running on ${port}`);
